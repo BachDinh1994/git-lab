5d042c6f50aea65d02e3d9dbc99f21a5ca9c66d7
Bach Dinh <bachdinh@guest-wireless-upc-nat-206-117-88-007.usc.edu>
2013-10-30 10:35:20 -0700

    merged and now I have Fizz & Buzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,13 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
+            <<<<<<< HEAD
             if( i % 3 == 0)
-                System.out.println(i + ": " + "Fizz");
+                System.out.println("Fizz");
+            =======
+            if( i % 5 == 0)
+                System.out.println("Buzz");
+            >>>>>>> d65d06f2547163e6a1d37d82fc5b283a010934eb
         }
     }
 }
